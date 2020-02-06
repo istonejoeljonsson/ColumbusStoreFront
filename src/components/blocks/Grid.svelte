fa27718bd3618d2c9e752b4ed314e61edfac13e3
Joel jonsson <joel.jonsson@istone.com>
2020-02-06 20:18:04 +0100

    CFS-2 - Added Grids

---
+++ src/components/blocks/Grid.svelte
@@ -0,0 +1,32 @@
+<style>
+ .grid-main{
+   @apply grid grid-cols-2 gap-4;
+   
+   @screen md{
+     @apply grid-cols-3;
+   }
+   
+   @screen lg{
+     @apply grid-cols-4;
+   }
+ }
+
+ .grid-child {
+	 @apply text-white bg-black;
+ }
+
+ img{
+   @apply w-full h-auto m-0 p-0;
+ }
+
+</style>
+
+
+  <div class="grid-main">
+    <div class="grid-child"><img src="https://placekitten.com/450/550" alt="Kitty" /></div>
+    <div class="grid-child"><img src="https://placekitten.com/450/550" alt="Kitty" /></div>
+    <div class="grid-child"><img src="https://placekitten.com/450/550" alt="Kitty" /></div>
+    <div class="grid-child"><img src="https://placekitten.com/450/550" alt="Kitty" /></div>
+    <div class="grid-child"><img src="https://placekitten.com/450/550" alt="Kitty" /></div>
+    <div class="grid-child"><img src="https://placekitten.com/450/550" alt="Kitty" /></div>
+  </div>
